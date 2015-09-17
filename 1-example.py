3429e894b1b5e26947e3fe53cbd33a3720ab0d54
Allen Leis <allen.leis@gmail.com>
2015-09-16 23:26:15 -0400

    initial import

---
+++ 1-example.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# 1-example
+# A simple demonstration script to find the largest prime number
+# below a given limit.
+#
+# This file is intentionally inefficient in order to demonstrate
+# various ways to time execution
+#
+# Usage:
+#   time python 1-example.py 10000
+#
+# Author:   Allen Leis <al1075@georgetown.edu>
+# Created:  Wed Sep 13 21:50:05 2015 -0400
+#
+# Copyright (C) 2015 georgetown.edu
+# For license information, see LICENSE.txt
+#
+# ID: 1-example.py [] al1075@georgetown.edu $
+
+"""
+A simple demonstration script to find the largest prime number
+below a given limit.
+"""
+
+##########################################################################
+## Imports
+##########################################################################
+
+import sys
+
+##########################################################################
+## Code
+##########################################################################
+
+def is_prime(limit):
+    """
+    Using the most time intensive method possible, return True or False
+    as to whether the supplied number is prime
+    """
+    for number in range(2,limit):
+        if (limit % number) == 0:
+            return False
+    return True
+
+
+def find_largest_prime(limit):
+    """
+    Find the highest number below the supplied limit/upper bound
+    that is a prime number
+    """
+    i = 2
+    largest_prime = None
+
+    while i < limit:
+        if is_prime(i):
+            largest_prime = i
+        i += 1
+    return largest_prime
+
+
+##########################################################################
+## Execution
+##########################################################################
+
+if __name__ == '__main__':
+    upper_bound = int(sys.argv[1])
+    print find_largest_prime(upper_bound)
+
---
+++ 4-example.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# 4-example
+# A simple demonstration script to find the largest prime number
+# below a given limit.
+#
+# This file is intentionally inefficient in order to demonstrate
+# the line_profiler module.  Read more at:
+#
+#     https://github.com/rkern/line_profiler
+#
+# Usage:
+#   kernprof -l -v 4-example.py 10000
+#
+# Author:   Allen Leis <al1075@georgetown.edu>
+# Created:  Wed Sep 13 21:50:05 2015 -0400
+#
+# Copyright (C) 2015 georgetown.edu
+# For license information, see LICENSE.txt
+#
+# ID: 4-example.py [] al1075@georgetown.edu $
+
+"""
+A simple demonstration script to find the largest prime number
+below a given limit.
+"""
+
+##########################################################################
+## Imports
+##########################################################################
+
+import sys
+
+##########################################################################
+## Code
+##########################################################################
+
+@profile
+def is_prime(limit):
+    """
+    Using the most time intensive method possible, return True or False
+    as to whether the supplied number is prime
+    """
+    for number in range(2,limit):
+        if (limit % number) == 0:
+            return False
+    return True
+
+@profile
+def find_largest_prime(limit):
+    """
+    Find the highest number below the supplied limit/upper bound
+    that is a prime number
+    """
+    i = 2
+    largest_prime = None
+
+    while i < limit:
+        if is_prime(i):
+            largest_prime = i
+        i += 1
+    return largest_prime
+
+
+##########################################################################
+## Execution
+##########################################################################
+
+if __name__ == '__main__':
+    upper_bound = int(sys.argv[1])
+    print find_largest_prime(upper_bound)
+
